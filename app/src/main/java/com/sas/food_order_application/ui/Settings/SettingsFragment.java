a041e81e35619047f0f5a8856f16a48bfd5b2923
Ashika.s <ashika.s@sasken.com>
2023-08-10 11:58:30 +0530

    "UI color DONE"

--- app/src/main/java/com/sas/food_order_application/ui/Settings/SettingsFragment.java
+++ app/src/main/java/com/sas/food_order_application/ui/Settings/SettingsFragment.java
@@ -22,19 +22,23 @@ import androidx.fragment.app.FragmentManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.sas.food_order_application.R;
 import com.sas.food_order_application.User_login;
 import com.sas.food_order_application.Welcome;
+import com.sas.food_order_application.admin.Admin_Logout;
 
 public class SettingsFragment extends Fragment {
-FirebaseAuth auth;
-FirebaseUser user;
+    FirebaseAuth auth;
+    FirebaseUser user;
     ListView listView;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    String[] SettingItems = new String[]{"Edit Profile","Delete Account","LogOut"};
+
+    String[] SettingItems = new String[]{"Edit Profile", "Delete Account", "LogOut"};
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,21 +53,23 @@ FirebaseUser user;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        listView=root.findViewById(R.id.list);
-        String emaill =  User_login.emailid;
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1,SettingItems);
+        listView = root.findViewById(R.id.list);
+        String emaill = User_login.emailid;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, SettingItems);
         listView.setAdapter(adapter);
+        Log.d("delete account", "DocumentSnapshot successfully deleted!" + emaill);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(position==2) {
+                if (position == 2) {
                     String selectedItem = (String) parent.getItemAtPosition(position);
                     showDialog();
-                } else if (position==0) {
-                    Intent intent=new Intent(getActivity().getApplicationContext(),MyprofileActivity.class);
+                } else if (position == 0) {
+                    Intent intent = new Intent(getActivity().getApplicationContext(), MyprofileActivity.class);
                     startActivity(intent);
-                } else
-                {
+                } else {
+
+                    dialog();
 //                    db.collection("Customer").document(emaill)
 //                            .delete()
 //                            .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -75,32 +81,76 @@ FirebaseUser user;
 //                            .addOnFailureListener(new OnFailureListener() {
 //                                @Override
 //                                public void onFailure(@NonNull Exception e) {
-//                                    Log.w("delete account", "Error deleting document", e);
+//                                    Log.d("delete account", "Error deleting document", e);
 //                                }
 //                            });
-//
-//                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//
-//                    if (user != null) {
-//                        user.delete()
-//                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                    @Override
-//                                    public void onComplete(@NonNull Task<Void> task) {
-//                                        if (task.isSuccessful()) {
-//                                            // User account deleted successfully.
-//                                            Log.d("delete", "User account deleted.");
-//                                        } else {
-//                                            // An error occurred.
-//                                            Log.e("delete", "Error deleting user account: " + task.getException().getMessage());
-//                                        }
-//                                    }
-//                                });
-//                    } else {
-//                        Log.d(TAG, "No user is currently signed in.");
-//                    }
-                    Toast.makeText(getActivity().getApplicationContext(),"non",Toast.LENGTH_SHORT).show();
+
                 }
             }
+
+            private void dialog() {
+
+
+                Context fragmentContext = requireContext();
+                AlertDialog.Builder builder = new AlertDialog.Builder(fragmentContext);
+                builder.setTitle("Are you sure you want to Delete Account?");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                      user = FirebaseAuth.getInstance().getCurrentUser();
+                      FirebaseFirestore db = FirebaseFirestore.getInstance();
+                      DocumentReference docRef = db.collection("Customer").document(emaill);
+                      Log.d("delete account", "successfully deleted!" + emaill);
+                      if (user != null) {
+                      docRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                    Log.d("delete account", "DocumentSnapshot successfully deleted!");
+                            }
+                      }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+                                Log.d("delete account", "Error deleting document", e);
+                        }
+                      });
+
+                      }
+
+                    if (user != null) {
+                    user.delete()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                // User account deleted successfully.
+                                Log.d("delete", "User account deleted.");
+
+                                } else {
+                                // An error occurred.
+                                Log.e("delete", "Error deleting user account: ");
+                                }
+                                }
+                                });
+                                } else {
+                                Log.d("delete", "No user is currently signed in.");
+                                }
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent=new Intent(getActivity().getApplicationContext(), Welcome.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    dialog.cancel();
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+
+
+
             private  void profile(){
             }
             private void showDialog() {
@@ -115,6 +165,9 @@ FirebaseUser user;
                         startActivity(intent);
                     }
                 });
+                builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    dialog.cancel();
+                });
                 AlertDialog alertDialog = builder.create();
                 alertDialog.show();
             }
@@ -122,3 +175,4 @@ FirebaseUser user;
         return root;
     }
 }
+
