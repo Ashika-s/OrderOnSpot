c34f9471a06c5057725ef5364ed4fba47deebe89
Manoj <ee212617@sasken.com>
2023-08-18 15:47:28 +0530

    btn decrease and increase count for item method correct but not working

--- app/src/main/java/com/sas/food_order_application/Adapter/HomeItemUserAdapter.java
+++ app/src/main/java/com/sas/food_order_application/Adapter/HomeItemUserAdapter.java
@@ -18,7 +18,9 @@ import com.sas.food_order_application.Model.HomeItemUserModel;
 import com.sas.food_order_application.Model.QuantityButton;
 import com.sas.food_order_application.R;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HomeItemUserAdapter extends RecyclerView.Adapter<HomeItemUserAdapter.ViewHolder> {
 
@@ -26,14 +28,11 @@ public class HomeItemUserAdapter extends RecyclerView.Adapter<HomeItemUserAdapte
     List<HomeItemUserModel> list;
     int[] minteger = {0};
     List<QuantityButton> quantityButtonList;
+    static HashMap<String,Integer> hashMap = new HashMap<>();
 
     public HomeItemUserAdapter(Context context, List<HomeItemUserModel> list) {
         this.context = context;
         this.list = list;
-    }
-
-    public HomeItemUserAdapter() {
-
     }
 
     @NonNull
@@ -47,35 +46,74 @@ public class HomeItemUserAdapter extends RecyclerView.Adapter<HomeItemUserAdapte
         holder.imageDish.setImageResource(list.get(position).getImage());
         holder.txtDishName.setText(list.get(position).getDishName());
         holder.txtAmount.setText("Amount : "+list.get(position).getDishAmount());
-        holder.btnIncrease.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              //  int pos=getAdapterPosition();
-                if(position!=RecyclerView.NO_POSITION ){
-                    minteger[0] = Integer.parseInt(holder.txtItemCount.getText().toString())+1;
-                    Log.d("Item Count",minteger[0]+"txt"+holder.txtItemCount.getText().toString());
 
-                    holder.txtItemCount.setText(String.valueOf(minteger[0]));
-                    notifyItemChanged(position);
-
-                }
-
-            }
-        });
-
-        holder.btnDecrease.setOnClickListener(new View.OnClickListener() {
-            @Override
-
-            public void onClick(View v) {
-               // int pos=getAdapterPosition();
-                minteger[0] = Integer.parseInt(holder.txtItemCount.getText().toString());
-                if(position!=RecyclerView.NO_POSITION &&  minteger[0]>0) {
-                    minteger[0] = minteger[0] - 1;
-                    holder.txtItemCount.setText(String.valueOf(minteger[0]));
-                    notifyItemChanged(position);
-                }
-            }
-        });
+//        holder.btnIncrease.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                int value = 1;
+//                String tempName=holder.txtDishName.getText().toString();
+//                Log.d("quantity","is "+tempName);
+//                if(position!=RecyclerView.NO_POSITION) {
+//                    Log.d("quantity position", "is " + position);
+//                    if (!hashMap.containsKey(tempName)) {
+//                        hashMap.put(tempName, value);
+//                        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+//                            String key = entry.getKey();
+//                            Integer val = entry.getValue();
+//                            Log.d("quantity map", "is " + entry.getKey() + "value is " + val);
+//                        }
+//                    }
+//
+//                    else
+//                        {
+//
+//                            int currentvalue = hashMap.get(tempName);
+//                            hashMap.put(tempName, currentvalue + 1);
+//                            for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+//                                String key = entry.getKey();
+//                                Integer val = entry.getValue();
+//                                Log.d("quantity map", "is " + entry.getKey() + "value is " + val);
+//                            }
+//
+//                        }
+//
+//                        holder.txtItemCount.setText(String.valueOf(hashMap.get(tempName)));
+//                    Log.d("quantity map set", "is "+holder.txtItemCount.getText().toString());
+//                     //   notifyItemChanged(position);
+//                        notifyDataSetChanged();
+//                    }
+//                }
+//
+//
+//        });
+//
+//        holder.btnDecrease.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//
+//            public void onClick(View v) {
+//               // int pos=getAdapterPosition();
+//                int value = 1;
+//                 String tempName=holder.txtDishName.getText().toString();
+//                if(position!=RecyclerView.NO_POSITION &&  minteger[0]>0) {
+//                 if(!hashMap.containsKey(tempName)) {
+//                     hashMap.put(tempName,value);
+//
+//                 }else
+//                 {
+//                     int currentvalue=hashMap.get(tempName);
+//                     hashMap.put(tempName,currentvalue-1);
+//
+//                 }
+//               // minteger[0] = Integer.parseInt(holder.txtItemCount.getText().toString());
+//
+//                   // minteger[0] = minteger[0] - 1;
+//                    holder.txtItemCount.setText(hashMap.get(tempName));
+//                  //  notifyItemChanged(position);
+//                    notifyDataSetChanged();
+//                }
+//            }
+//        });
 
 //        notifyItemChanged(position);
 //        holder.btnDecrease.setOnClickListener(new View.OnClickListener() {
@@ -109,22 +147,16 @@ public class HomeItemUserAdapter extends RecyclerView.Adapter<HomeItemUserAdapte
 
     }
 
-    public void updateData(List<HomeItemUserModel> newItems) {
-        list.clear();
-        list.addAll(newItems);
-        notifyDataSetChanged();
-    }
-
     @Override
     public int getItemCount() {
         return list.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         ImageView imageDish;
         TextView txtDishName,txtAmount;
-            static public TextView txtItemCount;
+        TextView txtItemCount;
         Button btnDecrease,btnIncrease;
 
 
@@ -136,6 +168,84 @@ public class HomeItemUserAdapter extends RecyclerView.Adapter<HomeItemUserAdapte
             txtItemCount=itemView.findViewById(R.id.txtinteger_number);
             btnDecrease=itemView.findViewById(R.id.btndecrease);
             btnIncrease=itemView.findViewById(R.id.btnincrease);
+
+            btnIncrease.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    int value = 1;
+                    int position=getAdapterPosition();
+                    String tempName=txtDishName.getText().toString();
+                    Log.d("quantity","is "+tempName);
+                    if(position!=RecyclerView.NO_POSITION) {
+                        if (!hashMap.containsKey(tempName)) {
+                            hashMap.put(tempName, value);
+                            for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+                                String key = entry.getKey();
+                                Integer val = entry.getValue();
+                                Log.d("quantity map", "is " + entry.getKey() + "value is " + val);
+                            }
+                        }
+
+                        else
+                        {
+
+                            int currentvalue = hashMap.get(tempName);
+                            hashMap.put(tempName, currentvalue + 1);
+                            for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+                                String key = entry.getKey();
+                                Integer val = entry.getValue();
+                                Log.d("quantity map", "is " + entry.getKey() + "value is " + val);
+                            }
+
+                        }
+
+                        txtItemCount.setText(String.valueOf(hashMap.get(tempName)));
+                        Log.d("quantity map set", "is "+txtItemCount.getText().toString());
+
+                        notifyItemChanged(position);
+                    }
+                }
+
+
+            });
+
+            btnDecrease.setOnClickListener(new View.OnClickListener() {
+                @Override
+
+                public void onClick(View v) {
+                     int position=getAdapterPosition();
+                    int value = 1;
+                    String tempName=txtDishName.getText().toString();
+                    Log.d("quantity","is "+tempName);
+                    if(position!=RecyclerView.NO_POSITION && hashMap.get(tempName)>0) {
+                        Log.d("quantity","is "+hashMap.get(tempName));
+
+                            int currentvalue=hashMap.get(tempName);
+                            hashMap.put(tempName, currentvalue - 1);
+                            if (currentvalue==0){
+                                hashMap.remove(tempName);
+                                txtItemCount.setText(0);
+
+                            }
+                            else {
+                                Log.d("quantity map set", "is "+hashMap.get(tempName));
+                                txtItemCount.setText(hashMap.get(tempName));
+                                for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+                                    String key = entry.getKey();
+                                    Integer val = entry.getValue();
+                                    Log.d("quantity map", "is " + entry.getKey() + "value is " + val);
+
+                                }
+                            }
+
+
+                        Log.d("quantity map set", "is "+txtItemCount.getText().toString());
+                        notifyItemChanged(position);
+
+                    }
+                }
+            });
 
 
         }
--- app/src/main/java/com/sas/food_order_application/Model/QuantityButton.java
+++ app/src/main/java/com/sas/food_order_application/Model/QuantityButton.java
@@ -2,8 +2,8 @@ package com.sas.food_order_application.Model;
 
 public class QuantityButton {
 //
-//    String increase;
-//    String decrease;
+    String increase;
+    String decrease;
 public static String count;
     public String getCount() {
         return count;
@@ -12,31 +12,31 @@ public static String count;
     public void setCount(String count) {
         this.count = count;
     }
-
-    public void countadd() {
-        count=count+1;
-    }
-
+//
+//    public void countadd() {
+//        count=count+1;
+//    }
+//
 //    public void countminus() {
 //        count -= 1;
 //    }
 
 
-//    public String getIncrease() {
-//        return increase;
-//    }
-//
-//    public void setIncrease(String increase) {
-//        this.increase = increase;
-//    }
-//
-//    public String getDecrease() {
-//        return decrease;
-//    }
-//
-//    public void setDecrease(String decrease) {
-//        this.decrease = decrease;
-//    }
+    public String getIncrease() {
+        return increase;
+    }
+
+    public void setIncrease(String increase) {
+        this.increase = increase;
+    }
+
+    public String getDecrease() {
+        return decrease;
+    }
+
+    public void setDecrease(String decrease) {
+        this.decrease = decrease;
+    }
 
 
 
